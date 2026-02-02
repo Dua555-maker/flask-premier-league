9a8a436d08f30b280274109ee1520490071b4af8
Dua555-maker <zxzxdua@gmail.com>
2026-02-02 12:18:11 +0700

    add all templates

--- epl/__init__.py
+++ epl/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epl.sqlite'
+app.secret_key = b'45sad4as3e1hkosd'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from epl import routes,models
