57a285839d94afc713255414934187c19fd8c250
Dua555-maker <zxzxdua@gmail.com>
2026-02-09 11:17:51 +0700

    make blueprint

--- epl/__init__.py
+++ epl/__init__.py
@@ -1,12 +1,33 @@
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epl.sqlite'
+# app.secret_key = b'45sad4as3e1hkosd'
+
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
+# from epl import routes,models
+
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from epl.extensions import db, migrate
+from epl.core.routes import core_bp
+from epl.clubs.routes import clubs_bp
+from epl.players.routes import players_bp
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epl.sqlite'
-app.secret_key = b'45sad4as3e1hkosd'
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-from epl import routes,models
+def create_app():
+  app = Flask(__name__)
+  
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/premier_league_db'
+  app.secret_key = b'45sad4as3e1hkosd'
+  
+  db.init_app(app)
+  migrate.init_app(app,db)
+  
+  app.register_blueprint(core_bp, url_prefix='/')
+  app.register_blueprint(clubs_bp, url_prefix='/clubs')
+  app.register_blueprint(players_bp, url_prefix='/players')
+  
+  return app
